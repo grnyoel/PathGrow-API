85e0054309392f38d7ae4efa6bee9e2fc64a9b25
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-13 06:45:39 +0800

    feat: update  userController

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -1,46 +1,79 @@
-const getAllUsers = (req, res) => {
-  const data = {
-    id: "1",
-    name: 'Gerent',
-    username: 'jueviole',
-    email: 'jueviole@gmail.com',
-    password: 'jueviole'
+const usersModel = require('../model/usersModel')
+
+const getAllUsers = async (req, res) => {
+  try {
+    const [data] = await usersModel.getAllUsers();
+  
+    res.json({
+      message: 'GET All Users success',
+      data: data
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server ERROR',
+      serverMessage: error,
+    })
   }
-  res.json({
-    message: 'GET All Users success',
-    data: data
-  })
 }
 
-const createNewUser = (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: 'CREATE New User success',
-    data: req.body
-  })
+const createNewUser = async (req, res) => {
+  const { body } = req;
+
+  if (!body.name || !body.username || !body.email || !body.password) {
+    return res.status(400).json({
+      message: 'Please fill in all fields correctly',
+      data: null,
+    })
+  }
+
+  try {
+    await usersModel.createNewUser(body);
+    res.status(201).json({
+      message: 'CREATE New User success',
+      data: body
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server ERROR',
+      serverMessage: error,
+    })
+  }
 }
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
   const { userID } = req.params;
-  console.log('userID: ', userID);
-  res.json({
-    message: 'UPDATE User success',
-    data: req.body
-  })
+  const { body } = req;
+  try {
+    await usersModel.updateUser(body, userID);
+    res.json({
+      message: 'UPDATE User success',
+      data: {
+        id: userID,
+        ...body
+      }
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server ERROR',
+      serverMessage: error,
+    })
+  }
 }
 
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
   const { userID } = req.params;
-  res.json({
-    message: 'DELETE User success',
-    data: {
-      id: userID,
-      name: "Jue Gerent",
-      username: "jueviole",
-      email: "jueviole@gmail.com",
-      password: "jueviole"
-    }
-  })
+  try {
+    await usersModel.deleteUser(userID);
+    res.json({
+      message: 'DELETE User success',
+      data: null
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server ERROR',
+      serverMessage: error,
+    })
+  }
 }
 
 module.exports = {

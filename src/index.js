634a13f0cd0ac5044daa1c003fd29cc76e447959
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-12 22:12:13 +0800

    feat: index.js for Express setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
+
+const usersRoutes = require('./routes/usersRoutes')
+const middlewareLogRequest = require('./middleware/usersMiddleware')
 
 const app = express();
+
+app.use(middlewareLogRequest);
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use('/users', usersRoutes);
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
-});
+app.listen(3000, () => {
+  console.log('Server running on PORT:3000');
+})

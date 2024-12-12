4ed16cbcee4b8dfafb986611451820407c30c2d3
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-13 06:47:37 +0800

    feat: update SQL Query

--- src/model/usersModel.js
+++ src/model/usersModel.js
@@ -1,11 +1,33 @@
 const dbPool = require('../config/mysql')
 
 const getAllUsers = () => {
-  const sqlQuery = 'SELECT * FROM users';
+  const SQLQuery = 'SELECT * FROM users';
   
-  return dbPool.execute(sqlQuery);
+  return dbPool.execute(SQLQuery);
+}
+
+const createNewUser = (body) => {
+  const SQLQuery = `INSERT INTO users (name, username, email, password)
+                    VALUES ('${body.name}', '${body.username}', '${body.email}', '${body.password}')`;
+  return dbPool.execute(SQLQuery);
+}
+
+const updateUser = (body, userID) => {
+  const SQLQuery = `UPDATE users 
+                    SET name='${body.name}', username='${body.username}'
+                    WHERE id=${userID}`;
+  return dbPool.execute(SQLQuery);
+}
+
+const deleteUser = (userID) => {
+  const SQLQuery = `DELETE FROM users WHERE id=${userID}`;
+
+  return dbPool.execute(SQLQuery);
 }
 
 module.exports = {
   getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
 }

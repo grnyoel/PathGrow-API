3a54ab16ec551a47743c09daae2c5814a443eb91
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-14 17:30:20 +0800

    update Query & add findUserByEmailOrUsername

--- src/model/usersModel.js
+++ src/model/usersModel.js
@@ -1,33 +1,35 @@
-const dbPool = require('../config/mysql')
+const dbPool = require('../config/mysql');
 
 const getAllUsers = () => {
   const SQLQuery = 'SELECT * FROM users';
-  
   return dbPool.execute(SQLQuery);
-}
+};
 
 const createNewUser = (body) => {
   const SQLQuery = `INSERT INTO users (name, username, email, password)
-                    VALUES ('${body.name}', '${body.username}', '${body.email}', '${body.password}')`;
-  return dbPool.execute(SQLQuery);
-}
+                    VALUES (?, ?, ?, ?)`;
+  return dbPool.execute(SQLQuery, [body.name, body.username, body.email, body.password]);
+};
+
+const findUserByEmailOrUsername = (emailOrUsername) => {
+  const SQLQuery = `SELECT * FROM users WHERE email = ? OR username = ?`;
+  return dbPool.execute(SQLQuery, [emailOrUsername, emailOrUsername]);
+};
 
 const updateUser = (body, userID) => {
-  const SQLQuery = `UPDATE users 
-                    SET name='${body.name}', username='${body.username}'
-                    WHERE id=${userID}`;
-  return dbPool.execute(SQLQuery);
-}
+  const SQLQuery = `UPDATE users SET name = ?, username = ? WHERE id = ?`;
+  return dbPool.execute(SQLQuery, [body.name, body.username, userID]);
+};
 
 const deleteUser = (userID) => {
-  const SQLQuery = `DELETE FROM users WHERE id=${userID}`;
-
-  return dbPool.execute(SQLQuery);
-}
+  const SQLQuery = `DELETE FROM users WHERE id = ?`;
+  return dbPool.execute(SQLQuery, [userID]);
+};
 
 module.exports = {
   getAllUsers,
   createNewUser,
+  findUserByEmailOrUsername,
   updateUser,
   deleteUser,
-}
+};
